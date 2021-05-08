64d8cc426c2d64bc8d2b9ca1ca7d8da4377f6b2f
DevMahdi404 <mdmahdi404@gmail.com>
2021-05-08 14:42:48 +0600

    static root changed

--- djrboilerplate/settings/base.py
+++ djrboilerplate/settings/base.py
@@ -102,7 +102,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'frontend/build')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticroot')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
